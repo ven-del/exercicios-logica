c8812b6b315461ab387685bf0e352941260efb3f
Wendell Régis <r.wendell.regis@gmail.com>
2025-05-01 13:29:44 +0000

    Adicionando segunda versão da resolução da questão 1

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
-function parOuImpar () {
+function parOuImpar() {
     const numberQuestion0 = Number(document.getElementById("number-question-0").value);
     const resultQuestion0 = document.getElementById("result-question-0");
-    numberQuestion0 % 2 == 0 ? resultQuestion0.innerHTML = `O número  ${numberQuestion0} é par.` : resultQuestion0.innerHTML = "O número é ímpar";
+    numberQuestion0 % 2 == 0 ? resultQuestion0.innerHTML = `O número  ${numberQuestion0} é par.` : resultQuestion0.innerHTML = `O número ${numberQuestion0} é ímpar`;
 }
+
+window.parOuImpar = parOuImpar;
