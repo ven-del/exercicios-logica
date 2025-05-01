9bf0201dd0f1aa95047e99c3c89be5f2ef0336e9
Wendell Régis <r.wendell.regis@gmail.com>
2025-05-01 13:18:48 +0000

    Adicionada resolução da questão 1 e adicionadas configurações de personalização no editor do codespaces.

---
+++ app.js
@@ -0,0 +1,5 @@
+function parOuImpar () {
+    const numberQuestion0 = Number(document.getElementById("number-question-0").value);
+    const resultQuestion0 = document.getElementById("result-question-0");
+    numberQuestion0 % 2 == 0 ? resultQuestion0.innerHTML = `O número  ${numberQuestion0} é par.` : resultQuestion0.innerHTML = "O número é ímpar";
+}
